9a314c7e68a478f720e96f68211b682a092f8881
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-02-19 13:23:17 +0800

    Created initial implementations of Ingredient, Recipe, and RecipeIngredient models with their functions

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.urls import reverse
 
+class Ingredient(models.Model):
+    name = models.CharField()
+
+class Recipe(models.Model):
+    name = models.CharField()
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField()
+    ingredient = models.ForeignKey(Ingredient, 
+                                   on_delete=models.CASCADE, 
+                                   related_name="ingredients")
+    recipe = models.ForeignKey(Recipe,
+                               on_delete=models.CASCADE, 
+                               related_name="recipes")
+    
+    def ingredient_str(self):
+        return self.ingredient
+    def recipe_str(self):
+        return self.recipe
+    def ingredient_absolute_url(self):
+        return reverse('ingredient_name', args=[str(self.ingredient)])
+    def recipe_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.recipe)])
 # Create your models here.
