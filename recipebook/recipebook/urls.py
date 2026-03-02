92c5ad95a7433402fae7bda87c71820a7299c007
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-02 14:51:57 +0800

    Added the accounts/ url for the custom login flow

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('ledger.urls'), name="ledger"),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
