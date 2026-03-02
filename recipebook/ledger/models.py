4d95b31bda564b39a5d32fd29a3432ace5d86873
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-02 08:52:19 +0800

    Created the Profile model with user, name, short_bio fields

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.CharField(validators=[MinLengthValidator(256)])
+
+    def __str__(self):
+        return self.user.username
 
 class Ingredient(models.Model):
     name = models.CharField()
