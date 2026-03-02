148ae8723d6550b9bb1c95b865ba7fd2d68b68e2
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-02 08:17:16 +0800

    Modified the URL pointer for the Ingredient model get_absolute_url functions's reverse parameter

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -9,7 +9,7 @@ class Ingredient(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("ledger:recipe_detail", args=[str(self.pk)])
+        return reverse("ingredient_detail", args=[str(self.pk)])
 
 
 class Recipe(models.Model):
