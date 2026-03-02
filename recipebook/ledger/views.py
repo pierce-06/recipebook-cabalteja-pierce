de9a550106d0948a90e73150824a67600ca88bbb
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-02 09:12:47 +0800

    Modified the Recipe Detail page to restrict access to looged in users

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .models import Recipe
-
+from django.contrib.auth.decorators import login_required
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipe/recipelist.html', {'recipes': recipes})
 
-
+@login_required
 def recipe_detail(request, pk):
     recipe = Recipe.objects.get(pk=pk)
     return render(request, 'recipe/recipedetail.html', {'recipe': recipe})
