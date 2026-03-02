652d23e0a50b83a4f20a2bfb05e2b6165cce1549
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-02 08:30:53 +0800

    Fixed wrong name for the RecipeIngredient model foreign key related name for Recipe and adjusted recipedetail.html to accommodate for the change

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -28,7 +28,7 @@ class RecipeIngredient(models.Model):
         Ingredient, on_delete=models.CASCADE, related_name="recipe"
     )
     recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name="ingredient"
+        Recipe, on_delete=models.CASCADE, related_name="ingredients"
     )
 
     def __str__(self):
