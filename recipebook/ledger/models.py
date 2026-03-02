919ca29ac4d68be9136b223180bddc0b44468c16
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-02 17:31:12 +0800

    Change value of the minimum length of the short_bio field from 255 to 256 characters

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -6,7 +6,7 @@ from django.core.validators import MinLengthValidator
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    short_bio = models.TextField(validators=[MinLengthValidator(255)])
+    short_bio = models.TextField(validators=[MinLengthValidator(256)])
 
     def __str__(self):
         return self.user.username
