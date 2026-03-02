b4f155783d5fce49a49a5f44401581d18cd00713
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-02 19:03:15 +0800

    Added EMAIL_BACKEND for the password reset flow

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'recipebook.urls'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
